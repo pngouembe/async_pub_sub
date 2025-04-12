8cd5e20eda0de6dccf9782ed5abac87e0230fbd0
Paul Ngouembe <paul.ngouembe@gmail.com>
2025-04-12 11:25:17 +0200

    refactor: refactor publisher macro

--- async_pub_sub_macros/src/publisher.rs
+++ async_pub_sub_macros/src/publisher.rs
@@ -1,38 +1,141 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use std::{collections::HashMap, fmt::Debug};
 use syn::DeriveInput;
 
-use crate::helpers::find_all_publisher_fields;
-
 pub(crate) fn derive_publisher_impl(input: DeriveInput) -> TokenStream {
-    let struct_name = &input.ident;
+    InputStruct::try_from(input)
+        .and_then(|input| Ok(input.generate_code()))
+        .unwrap_or_else(|err| err.to_compile_error())
+        .into()
+}
 
-    let fields = match &input.data {
-        syn::Data::Struct(data) => match &data.fields {
-            syn::Fields::Named(fields_named) => &fields_named.named,
-            _ => panic!("Only named fields are supported"),
-        },
-        _ => panic!("Only structs are supported"),
-    };
+struct InputStruct {
+    context: CodeGenerationContext,
+    publisher_fields: Vec<PublisherField>,
+    is_multi_publisher: bool,
+}
 
-    let publisher_fields = find_all_publisher_fields(fields, &input);
-    if publisher_fields.is_empty() {
-        panic!("Struct must have at least one field that implements the Publisher trait or is marked with #[publisher]");
+impl TryFrom<DeriveInput> for InputStruct {
+    type Error = syn::Error;
+
+    fn try_from(input: DeriveInput) -> Result<Self, Self::Error> {
+        let context = CodeGenerationContext {
+            struct_name: input.ident.clone(),
+            generics: input.generics.clone(),
+        };
+
+        let publisher_fields = find_all_publishers(&input)?;
+
+        if publisher_fields.is_empty() {
+            return Err(syn::Error::new_spanned(
+                input,
+                "DerivePublisher macro requires that your struct must have at least one field that implements the Publisher trait or is marked with #[publisher]",
+            ));
+        }
+
+        let is_multi_publisher = publisher_fields.len() > 1;
+        Ok(Self {
+            context,
+            publisher_fields,
+            is_multi_publisher,
+        })
+    }
+}
+
+impl InputStruct {
+    fn generate_code(&self) -> proc_macro2::TokenStream {
+        if self.is_multi_publisher {
+            self.publisher_fields
+                .iter()
+                .fold(quote! {}, |generated_code, publisher_field| {
+                    let impl_code = publisher_field.generate_wrapper_impl(&self.context);
+                    quote! {
+                        #generated_code
+                        #impl_code
+                    }
+                })
+        } else {
+            let publisher_field = self
+                .publisher_fields
+                .first()
+                .expect("publisher_fields is not empty, this should never happen");
+
+            publisher_field.generate_impl(&self.context)
+        }
+    }
+}
+
+struct CodeGenerationContext {
+    struct_name: syn::Ident,
+    generics: syn::Generics,
+}
+
+struct PublisherField {
+    name: proc_macro2::TokenStream,
+    message_type: proc_macro2::TokenStream,
+}
+
+impl Debug for PublisherField {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PublisherField")
+            .field("name", &self.name.to_string())
+            .field("message_type", &self.message_type.to_string())
+            .finish()
+    }
+}
+
+impl PublisherField {
+    fn new(name: proc_macro2::TokenStream, message_type: proc_macro2::TokenStream) -> Self {
+        Self { name, message_type }
     }
 
-    let generics = &input.generics;
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    fn from_field_attributes_opt(field: &syn::Field) -> Option<Self> {
+        let message_type = field
+            .attrs
+            .iter()
+            .find(|attr| attr.path().is_ident("publisher"))
+            .and_then(|attr| {
+                let ty = attr.parse_args::<syn::Type>().ok()?;
+                Some(quote! { #ty })
+            })?;
 
+        let field_name = field.ident.clone().map(|ident| quote! { #ident })?;
 
-    let expanded = if publisher_fields.len() == 1 {
-        // Single publisher case - implement Publisher trait
-        let (field, message_type) = publisher_fields.first().expect("Publisher_fields is not empty, this should never happen");
-        let field_name = &field.ident;
+        Some(Self::new(field_name, message_type))
+    }
+
+    fn from_field_type_opt(
+        field: &syn::Field,
+        generic_publishers: &HashMap<syn::Ident, proc_macro2::TokenStream>,
+    ) -> Option<Self> {
+        let syn::Type::Path(syn::TypePath { path, .. }) = &field.ty else {
+            return None;
+        };
+
+        let Some(ident) = path.get_ident() else {
+            return None;
+        };
+
+        let message_type = generic_publishers
+            .get(ident)
+            .cloned()
+            .or_else(|| publisher_message_type_from_path_opt(path))?;
+
+        let field_name = field.ident.clone().map(|ident| quote! { #ident })?;
+
+        Some(Self::new(field_name, message_type))
+    }
+
+    fn generate_impl(&self, context: &CodeGenerationContext) -> proc_macro2::TokenStream {
+        let field_name = &self.name;
+        let message_type = &self.message_type;
+        let struct_name = &context.struct_name;
+        let (impl_generics, ty_generics, where_clause) = &context.generics.split_for_impl();
 
         quote! {
             impl #impl_generics async_pub_sub::Publisher for #struct_name #ty_generics #where_clause {
                 type Message = #message_type;
-
 
                 fn get_name(&self) -> &'static str {
                     async_pub_sub::Publisher::get_name(&self.#field_name)
@@ -50,30 +153,213 @@ pub(crate) fn derive_publisher_impl(input: DeriveInput) -> TokenStream {
                 }
             }
         }
-    } else {
-        // Multiple publishers case - implement PublisherWrapper trait for each message type
-        let impls = publisher_fields.iter().map(|(field, message_type)| {
-            let field_name = &field.ident;
+    }
 
-            quote! {
-                impl #impl_generics async_pub_sub::PublisherWrapper<#message_type> 
-                    for #struct_name #ty_generics #where_clause 
-                {
-                    fn get_publisher(&self) -> &impl async_pub_sub::Publisher<Message = #message_type> {
-                        &self.#field_name
-                    }
-
-                    fn get_publisher_mut(&mut self) -> &mut impl async_pub_sub::Publisher<Message = #message_type> {
-                        &mut self.#field_name
-                    }
-                }
-            }
-        });
+    fn generate_wrapper_impl(&self, context: &CodeGenerationContext) -> proc_macro2::TokenStream {
+        let field_name = &self.name;
+        let message_type = &self.message_type;
+        let struct_name = &context.struct_name;
+        let (impl_generics, ty_generics, where_clause) = &context.generics.split_for_impl();
 
         quote! {
-            #(#impls)*
+            impl #impl_generics async_pub_sub::PublisherWrapper<#message_type>
+            for #struct_name #ty_generics #where_clause {
+                fn get_publisher(&self) -> &impl async_pub_sub::Publisher<Message = #message_type> {
+                    &self.#field_name
+                }
+
+                fn get_publisher_mut(&mut self) -> &mut impl async_pub_sub::Publisher<Message = #message_type> {
+                    &mut self.#field_name
+                }
+            }
+        }
+    }
+}
+
+fn find_all_publishers(input: &DeriveInput) -> Result<Vec<PublisherField>, syn::Error> {
+    let mut generic_publishers = find_publishers_in_generic_type_params(input);
+    generic_publishers.extend(find_publishers_in_where_clauses(input).into_iter());
+
+    let fields = match &input.data {
+        syn::Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(fields_named) => &fields_named.named,
+            _ => {
+                return Err(syn::Error::new_spanned(
+                    &input,
+                    "DerivePublisher macro only supports structs with named fields",
+                ))
+            }
+        },
+        _ => {
+            return Err(syn::Error::new_spanned(
+                input,
+                "DerivePublisher macro only supports structs",
+            ))
         }
     };
 
-    TokenStream::from(expanded)
+    let publisher_fields = fields
+        .iter()
+        .filter_map(|field| {
+            PublisherField::from_field_attributes_opt(field).or(
+                PublisherField::from_field_type_opt(field, &generic_publishers),
+            )
+        })
+        .collect();
+
+    Ok(publisher_fields)
+}
+
+fn find_publishers_in_generic_type_params(
+    input: &DeriveInput,
+) -> HashMap<syn::Ident, proc_macro2::TokenStream> {
+    input
+        .generics
+        .type_params()
+        .filter_map(try_type_param_to_publisher_ident_message_type_pair)
+        .collect()
+}
+
+fn try_type_param_to_publisher_ident_message_type_pair(
+    type_param: &syn::TypeParam,
+) -> Option<(syn::Ident, proc_macro2::TokenStream)> {
+    let syn::TypeParam { ident, bounds, .. } = type_param;
+
+    bounds.iter().find_map(|bound| {
+        let syn::TypeParamBound::Trait(trait_bound) = bound else {
+            return None;
+        };
+
+        let path = &trait_bound.path;
+
+        if path.is_ident("Publisher") {
+            return Some((
+                ident.clone(),
+                quote! { <#ident as async_pub_sub::Publisher>::Message },
+            ));
+        }
+
+        let message_type = bounds.iter().find_map(|bound| {
+            let syn::TypeParamBound::Trait(trait_bound) = bound else {
+                return None;
+            };
+
+            let path = &trait_bound.path;
+
+            if path.is_ident("Publisher") {
+                return Some(quote! { <#ident as async_pub_sub::Publisher>::Message });
+            }
+
+            path.segments
+                .iter()
+                .find_map(publisher_message_type_from_path_segment_opt)
+                .or(Some(
+                    quote! { <#ident as async_pub_sub::Publisher>::Message },
+                ))
+        })?;
+
+        Some((type_param.ident.clone(), message_type))
+    })
+}
+
+fn find_publishers_in_where_clauses(
+    input: &DeriveInput,
+) -> HashMap<syn::Ident, proc_macro2::TokenStream> {
+    input
+        .generics
+        .where_clause
+        .as_ref()
+        .map(|where_clause| {
+            where_clause
+                .predicates
+                .iter()
+                .filter_map(try_where_predicate_to_publisher_ident_message_type_pair)
+                .collect()
+        })
+        .unwrap_or_default()
+}
+
+fn try_where_predicate_to_publisher_ident_message_type_pair(
+    predicate: &syn::WherePredicate,
+) -> Option<(syn::Ident, proc_macro2::TokenStream)> {
+    let syn::WherePredicate::Type(syn::PredicateType {
+        bounded_ty, bounds, ..
+    }) = predicate
+    else {
+        return None;
+    };
+
+    let syn::Type::Path(syn::TypePath { path, .. }) = bounded_ty else {
+        return None;
+    };
+
+    let Some(ident) = path.get_ident() else {
+        return None;
+    };
+
+    let message_type = bounds.iter().find_map(|bound| {
+        let syn::TypeParamBound::Trait(trait_bound) = bound else {
+            return None;
+        };
+
+        let path = &trait_bound.path;
+
+        if path.is_ident("Publisher") {
+            return Some(quote! { <#ident as async_pub_sub::Publisher>::Message });
+        }
+
+        path.segments
+            .iter()
+            .find_map(publisher_message_type_from_path_segment_opt)
+            .or(Some(
+                quote! { <#ident as async_pub_sub::Publisher>::Message },
+            ))
+    })?;
+
+    Some((ident.clone(), message_type))
+}
+
+fn publisher_message_type_from_path_opt(path: &syn::Path) -> Option<proc_macro2::TokenStream> {
+    let ident = path.get_ident()?;
+
+    if ident != "Publisher" {
+        return None;
+    }
+
+    if path.is_ident("Publisher") {
+        return Some(quote! { <#ident as async_pub_sub::Publisher>::Message });
+    }
+
+    path.segments
+        .iter()
+        .find_map(publisher_message_type_from_path_segment_opt)
+        .or(Some(
+            quote! { <#ident as async_pub_sub::Publisher>::Message },
+        ))
+}
+
+fn publisher_message_type_from_path_segment_opt(
+    segment: &syn::PathSegment,
+) -> Option<proc_macro2::TokenStream> {
+    let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments { args, .. }) =
+        &segment.arguments
+    else {
+        return None;
+    };
+
+    args.iter().find_map(|arg| {
+        let syn::GenericArgument::AssocType(assoc_ty) = arg else {
+            return None;
+        };
+
+        if assoc_ty.ident != "Message" {
+            return None;
+        }
+
+        let syn::Type::Path(syn::TypePath { path, .. }) = &assoc_ty.ty else {
+            return None;
+        };
+
+        path.get_ident().map(|ident| quote! { #ident })
+    })
 }
